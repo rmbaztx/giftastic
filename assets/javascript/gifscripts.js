4b021066b7cd99a37d2ea5dbd64bf32a7dfd2973
rmbaztx <rmbloodgood@mac.com>
2018-04-30 19:48:18 -0500

    Initial submission of homework assignment

--- assets/javascript/gifscripts.js
+++ assets/javascript/gifscripts.js
@@ -3,10 +3,8 @@
 
 //remember to add line that makes sure html is ready
 //
-$(document).ready(function(){ //would moving this have any impact on inop buttons?
+$(document).ready(function(){ 
 //initialize vars
-var a = "test"; //just used to verify correct paths to javascript, style, etc.
-console.log(a);
 var transportationType = "";
 var topics = [
     "classic cars",
@@ -18,7 +16,23 @@ var topics = [
     "airplanes"
 ];
 //create initial topic buttons in div "gifs-appear-here" 
-
+ function watchGifs() {
+            $(".gif").on("click", function() {
+            // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+            var state = $(this).attr("data-state");
+            console.log(state);
+            // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+            // Then, set the image's data-state to animate
+            // Else set src to the data-still value
+            if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+            } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+            }
+        })
+    }
 //add line to ignore default clearing/refresh of screen (may go below button.click)
 function renderButtons() {
     $("#topic-view").empty();
@@ -30,15 +44,16 @@ for (var i = 0;i < topics.length;i++) {
     $("#topic-view").append(a);
     console.log("topic " + i + " = " + topics[i]);
 }
-   
+watchButtons();
+watchGifs();   
 }
 //listen to the class of the topic buttons; when one is clicked, use 'this' to build and run
 //the Giphy query using Ajax
 renderButtons();
 //try changing "button" to "#mode" - buttons do not work; .mode: buttons work until I add 
 // a new topic; should this be function() or function(event)?
-//function watchButtons() {
-    $(".mode").on("click", function() {
+function watchButtons() {
+    $(".mode").on("click", function(event) {
     console.log("topic button clicked");
     var mode = $(this).attr("transportationType");
     
@@ -83,6 +98,8 @@ renderButtons();
         // filename will be fixed_height or fixed_height_still for 200 x 200
         // animated or still
         modeImage.attr("src", results[i].images.fixed_height_still.url);
+        modeImage.attr('data-animate', results[i].images.fixed_height.url);
+        modeImage.attr('data-still', results[i].images.fixed_height_still.url);
         modeImage.attr("data-state","still");
         modeImage.attr("class", "gif");
 
@@ -93,15 +110,15 @@ renderButtons();
 
         // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
         $("#gifs-appear-here").prepend(gifDiv);
-
       }
     }
+    watchGifs();
 })
     //renderButtons();
 });
-//})
-//assign image's url to the image
-
+}
+       
+    
 //toggle between animated and still images (see pausing gifs)
 //Watch for a class = "gif" button click, change img src to still or animate
 //by changing src=filename and setting flag to still or animate
@@ -115,8 +132,6 @@ renderButtons();
 
       // This function handles events where button to add a new topic is clicked
       $("#add-topic").on("click", function(event) {
-        // event.preventDefault() prevents the form from trying to submit itself.
-        // We're using a form so that the user can hit enter instead of clicking the button if they want
         event.preventDefault(); //new button would only briefly display without this line
 
         // This line will grab the text from the input box
@@ -126,12 +141,14 @@ renderButtons();
         console.log("Number of topics = " + topics.length); //OK
         //clear the button
         // $("#topic-input").empty(); //this did not work
-        $("#topic-input").text("");
+        $("#topic-input").val("");
         // calling renderButtons which handles the processing of our topics array
         // I believe this belongs here, but the buttons appear disabled after trying
         // to enter a new topic button - even though the button is generated
         renderButtons(); //OK to here; why don't topic button clicks work after this?????
-      })
+        
+      
+    })
       });
     
 // I suspect the problem is where I have the on.click monitoring - do they need to be in  
